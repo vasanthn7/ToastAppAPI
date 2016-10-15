d8a79063695d3c2c36a42853e63c12ab4ec3ac00
Vasanth <vasanthn7@gmail.com>
2016-10-15 15:06:54 +0530

    ToastApp_2nd_working

--- getdetail.py
+++ getdetail.py
@@ -12,11 +12,40 @@ class get_detail(object):
         print type(self.id)
         page = requests.get('http://food2fork.com/api/get?key='+api_key+'&rId='+str(self.id))
         parsed = json.loads(page.text)
-        tag = tags(parsed['recipe']['publisher'])
+        publisher = parsed['recipe']['publisher']
         print parsed['recipe']['publisher']
+        if (publisher == "All Recipes"):
+    		tag = '//span[@class="recipe-directions__list--item"]/text()'
+    	elif (publisher == "Back to Her Roots"):
+    		tag = '//li[@class="instruction"]/text()'
+    	elif (publisher == "BBC Food"):
+    		tag = '//p[@class="recipe-method__list-item-text"]/text()'
+    	elif (publisher == "Bon Appetit"):
+    		tag = '//div[@class="text" and @itemprop="recipeInstructions"]/text()'
+    	elif (publisher == "Closet Cooking"):
+    		tag = '//li[@itemprop="recipeInstructions"]/text()'
+    	elif (publisher == "Cookin Canuck"):
+    		tag = '//li[@class="instruction" and @itemprop="recipeInstructions"]/text()'
+    	elif (publisher == "Epicurious"):
+    		tag = '//li[@class="preparation-step"]/text()'
+    	elif (publisher == "Epicurious"):
+    		tag = '//li[@class="preparation-step"]/text()'
+    	elif (publisher == "Food Republic"):
+    		tag = '//span[@itemprop="recipeInstructions"]/ol/li/text()'
+    	elif (publisher == "Framed Cooks"):
+    		tag = '//li[@class="instruction" and @itemprop="recipeInstructions"]/text()'
+    	elif (publisher == "Healthy Delicious"):
+    		tag = '//li[@class="instruction" and @itemprop="recipeInstructions"]/text()'
+    	elif (publisher == "Jamie Oliver"):
+    		tag = '//ol[@class="recipeSteps"]/li/text()'
+    	elif (publisher == "Lisa's Kitchen"):
+    		tag = '//li[@itemprop="recipeInstructions"]/p/text()'
+    	else:
+    		tag = 404
+
         if (tag == 404):
-            return 404
-            quit()
+            response = json.dumps({'Status':0},sort_keys=False,separators=(',',':'))
+            return str(response).replace("'",'')
         ingredients = parsed['recipe']['ingredients']
         # print ingredients
         url = parsed['recipe']['source_url']
@@ -24,9 +53,9 @@ class get_detail(object):
         #print page1.text
         tree = html.fromstring(page1.content)
         directions = tree.xpath(tag)
-        response = json.dumps({'Ingredients':ingredients,'Direction':directions},sort_keys=False,separators=(',',':'))
+        response = json.dumps({'Status':1,'Ingredients':ingredients,'Direction':directions},sort_keys=False,separators=(',',':'))
         return str(response).replace("'",'')
 
 if __name__ == '__main__':
-    obj = get_detail("35196")
+    obj = get_detail("35437")
     obj.return_detail()
